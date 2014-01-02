594125c383729648909943b996c129e24f18f457
Carlos Cerrillos <carlos@cerrillostech.com>
2014-01-01 19:15:31 -0800

    Removed obsolete code

--- inc/admin.inc.php
+++ inc/admin.inc.php
@@ -63,46 +63,6 @@ function showElement(element_id) {
 												</header>
                                             	<?
 												if(isset($_SESSION["is_admin"])){
-													/*
-												?>
-                                                <table cellspacing="10px">
-                                                	<tr>
-                                                    	<td>
-                                                            <div id="quizmanagement" style="margin-left:auto; margin-right:auto">
-                                                            	<center>
-                                                                    <h4 style="margin-bottom:0;">Quiz Management</h4>
-                                                                    <a href="?p=quizadmin" class=""><i class="fa fa-cog"></i> Edit Quizzes</a>
-                                                                </center>
-                                                        	</div>
-                                                        </td>
-                                                        <td>
-                                                            <div id="results">
-                                                            	<center>
-                                                                    <h4 style="margin-bottom:0;">Results & Grading</h4>
-                                                                    <a href="?p=results" class=""><i class="fa fa-cog"></i> Quiz Results</a><br />
-                                                                    <a href="?p=gradefr" class=""><i class="fa fa-cog"></i> Pending Free<br />Response Grading</a>
-                                                                </center>
-                                                            </div>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                    	<td>
-                                                        <div id="sessions">
-                                                        <center>
-                                                        <h4 style="margin-bottom:0;">Session Management</h4>
-                                                        <a href="?p=sessions" class=""><i class="fa fa-cog"></i> Edit Sessions</a><br>
-                                                        <a href="?p=pendingsessions" class=""><i class="fa fa-cog"></i> Quizzes In Progress</a>
-                                                        </center>
-                                                    	</div>
-                                                        </td>
-                                                        <td></td>
-                                                    </tr>
-                                                </table>
-                                                	
-                                                    
-                                                    
-												<?
-												*/
 												}
 												?>
                                                 <img src="http://imgs.xkcd.com/comics/computer_problems.png" title="This is how I explain computer problems to my cat. My cat usually seems happier than me." alt="Computer Problems">
