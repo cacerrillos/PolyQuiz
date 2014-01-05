98f32a94a2ef557989079cca3be5159c742a8044
Carlos Cerrillos <carlos@cerrillostech.com>
2014-01-04 21:32:27 -0800

    Change count script to not include phpmyadmin

--- count.php
+++ count.php
@@ -1,4 +1,4 @@
 <?
 #Counts lines of code in .php files
-echo shell_exec("find . -name '*.php' -not -path './securimage/*' | xargs wc -l");
+echo shell_exec("find . -name '*.php' -not -path './securimage/*' -not -path './phpmyadmin/*' | xargs wc -l");
 ?>
