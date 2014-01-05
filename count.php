b7e91532a4250a710ba91e5d97e05024f52d1f43
Carlos Cerrillos <carlos@cerrillostech.com>
2014-01-04 21:45:39 -0800

    Used a preg match to simplify the output of count.php

--- count.php
+++ count.php
@@ -1,4 +1,6 @@
 <?
 #Counts lines of code in .php files
-echo shell_exec("find . -name '*.php' -not -path './securimage/*' -not -path './phpmyadmin/*' | xargs wc -l");
+$string = shell_exec("find . -name '*.php' -not -path './securimage/*' -not -path './phpmyadmin/*' | xargs wc -l");
+preg_match_all('/[0-9]+/',$string,$matches);
+echo $matches[0][count($matches[0])-1]." Lines of Code in PolyQuiz!";
 ?>
