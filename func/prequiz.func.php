a7d8ab1f610eeee290b38fabb9d16137a47490b1
Carlos Cerrillos <carlos@cerrillostech.com>
2014-05-18 16:08:01 -0700

    Removed more obsolete code

--- func/prequiz.func.php
+++ func/prequiz.func.php
@@ -7,54 +7,35 @@ if(isset($_POST['submit'])){
 	if($_POST['submit']=="Begin"){
 		$firstname = $_POST['firstname'];
 		$lastname = $_POST['lastname'];
-		$type = $_POST['type'];
-		if($type=="normal"){
-			$uuid = $_POST['uuid'];
-			$house = $_POST['house'];
-			if(strlen($firstname)>0 && strlen($lastname)>0 && isset($_POST['house'])){
-				$_SESSION['firstname'] = $_POST['firstname'];
-				$_SESSION['lastname'] = $_POST['lastname'];
-				$_SESSION['house'] = $house;
-				$temp = new quizFromMysql();
-				$generatedquiz = $temp -> createQuiz($uuid);
-				$generatedquiz -> randomize();
-				if($generatedquiz==false){
-					header('Location: ' . $_SERVER['HTTP_REFERER']);
-				} else {
-					$_SESSION['quiz'] = $generatedquiz;
+		if(strlen($firstname)>0 && strlen($lastname)>0 && strlen($_POST['sessionid'])>0 && strlen($_POST['sessionkey'])>0){
+			$sessionid = $_POST['sessionid'];
+			$sessionkey = $_POST['sessionkey'];
+			$qsfm = new quizSessionFromMysql();
+			$tempqs = $qsfm -> getSession(getDBExt($sessionid), $sessionid, $sessionkey);
+			if($tempqs==false){
+				header('Location: ' . $_SERVER['HTTP_REFERER']."");
+			} else {
+				if($tempqs!=false){
+					$uuid = $tempqs->getQuizes();
+					$house = $tempqs->getHouse();
 				}
-				header('Location: ' . $_SERVER['HTTP_REFERER']);
-			}
-			header('Location: ' . $_SERVER['HTTP_REFERER']);
-		} else {
-			if(strlen($firstname)>0 && strlen($lastname)>0 && strlen($_POST['sessionid'])>0 && strlen($_POST['sessionkey'])>0){
-				$sessionid = $_POST['sessionid'];
-				$sessionkey = $_POST['sessionkey'];
-				$qsfm = new quizSessionFromMysql();
-				$tempqs = $qsfm -> getSession(getDBExt($sessionid), $sessionid, $sessionkey);
-				if($tempqs==false){
-					header('Location: ' . $_SERVER['HTTP_REFERER']."");
-				} else {
-					if($tempqs!=false){
-						$uuid = $tempqs->getQuizes();
-						$house = $tempqs->getHouse();
+				if(strlen($firstname)>0 && strlen($lastname)>0){
+					$_SESSION['firstname'] = $_POST['firstname'];
+					$_SESSION['lastname'] = $_POST['lastname'];
+					$_SESSION['house'] = $house;
+					$_SESSION['dbext'] = getDBExt($sessionid);
+					if(isset($_SESSION['is_admin'])){
+						unset($_SESSION['is_admin']);
 					}
-					if(strlen($firstname)>0 && strlen($lastname)>0){
-						$_SESSION['firstname'] = $_POST['firstname'];
-						$_SESSION['lastname'] = $_POST['lastname'];
-						$_SESSION['house'] = $house;
-						$_SESSION['dbext'] = getDBExt($sessionid);
-						$temp = new quizFromMysql();
-						$generatedquiz = $temp -> createQuiz($uuid);
-						$generatedquiz -> randomize();
-						if($generatedquiz==false){
-							header('Location: ' . $_SERVER['HTTP_REFERER']);
-						} else {
-							$_SESSION['session'] = $tempqs;
-							$generatedquiz -> setSession($tempqs);
-							$_SESSION['quiz'] = $generatedquiz;
-							header('Location: ' . $_SERVER['HTTP_REFERER']);
-						}
+					$temp = new quizFromMysql();
+					$generatedquiz = $temp -> createQuiz($uuid);
+					$generatedquiz -> randomize();
+					if($generatedquiz==false){
+						header('Location: ' . $_SERVER['HTTP_REFERER']);
+					} else {
+						$_SESSION['session'] = $tempqs;
+						$generatedquiz -> setSession($tempqs);
+						$_SESSION['quiz'] = $generatedquiz;
 						header('Location: ' . $_SERVER['HTTP_REFERER']);
 					}
 					header('Location: ' . $_SERVER['HTTP_REFERER']);
@@ -63,6 +44,7 @@ if(isset($_POST['submit'])){
 			}
 			header('Location: ' . $_SERVER['HTTP_REFERER']);
 		}
+		header('Location: ' . $_SERVER['HTTP_REFERER']);
 	} else if($_POST['submit']=="Resume"){
 		$resumeid = $_POST['id'];
 		$resumekey = $_POST['key'];
