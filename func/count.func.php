54d7c781fb9aa826a5b526251ec0efbcae476e3b
Carlos Cerrillos <carlos@cerrillostech.com>
2014-05-20 22:26:13 -0700

    Formatting

--- func/count.func.php
+++ func/count.func.php
@@ -1,9 +1,9 @@
 <?
 #Counts lines of code in .php files
 function lines(){
-$string = shell_exec("find . -name '*.php' -not -path './securimage/*' -not -path './phpmyadmin/*' | xargs wc -l");
-preg_match_all('/[0-9]+/',$string,$matches);
-$linesofcode = $matches[0][count($matches[0])-1];
-return $linesofcode;
+	$string = shell_exec("find . -name '*.php' -not -path './securimage/*' -not -path './phpmyadmin/*' | xargs wc -l");
+	preg_match_all('/[0-9]+/',$string,$matches);
+	$linesofcode = $matches[0][count($matches[0])-1];
+	return $linesofcode;
 }
 ?>
