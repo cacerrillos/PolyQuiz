039a718d016ccfcb69957088ccc04eb2bbaf3720
Carlos Cerrillos <carlos@cerrillostech.com>
2014-05-20 22:29:32 -0700

    Added $_SESSION variable for user id's

--- func/admin.login.php
+++ func/admin.login.php
@@ -9,24 +9,25 @@ if($_POST['submit']=="Login"){
 		exit();
 	}
 	if($stmt = $mysqli -> prepare("SELECT * FROM users WHERE username = ? AND password = ?")){
-			$stmt -> bind_param("ss", $_POST['user'], md5($_POST['pass']));
-			$stmt -> execute();
-			$stmt -> bind_result($data['id'], $data['username'], $data['passwordHash'], $data['permsid']);
-			$stmt -> store_result();
-			$stmt -> fetch();
-			$num = $stmt -> num_rows;
-			$stmt -> close();
-		} else {
-			echo $mysqli->error;
-		}
+		$stmt -> bind_param("ss", $_POST['user'], md5($_POST['pass']));
+		$stmt -> execute();
+		$stmt -> bind_result($data['id'], $data['username'], $data['passwordHash'], $data['permsid']);
+		$stmt -> store_result();
+		$stmt -> fetch();
+		$num = $stmt -> num_rows;
+		$stmt -> close();
+	} else {
+		echo $mysqli->error;
+	}
+	$mysqli -> close();
 	if($num==1){
 		$_SESSION['is_admin'] = "set";
 		$_SESSION['username'] = $_POST['user'];
 		$_SESSION['dbext'] = "_".$data['permsid'];
+		$_SESSION['admin_id_num'] = $data['id'];
 		$_SESSION['permsid'] = $data['permsid'];
 		header('Location: ' . $_SERVER['HTTP_REFERER']);
 	}
-	$mysqli -> close();
 }
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 ?>
