9ba20ba87caf2daa3784b6b61127f1209c1f6b14
Carlos Cerrillos <carlos@cerrillostech.com>
2014-06-15 20:22:42 -0700

    Updated gradingfr.func.php for new Schema

--- func/gradingfr.func.php
+++ func/gradingfr.func.php
@@ -7,18 +7,18 @@ if(isset($_SESSION["is_admin"])){
 	if(isset($_POST['submit'])){
 		$uuid = $_POST['uuid'];	
 		$mysqli = new mysqli($db_host, $db_user, $db_password);
-		$mysqli -> select_db($_SESSION['dbext']);
+		$mysqli -> select_db($db_name);
 		if(mysqli_connect_errno()){
 			echo "Connection Failed: " . mysqli_connect_errno();
 			exit();
 		}
 		$numrows = 0;
-		if($stmt = $mysqli -> prepare("SELECT * FROM results WHERE id=?")){
-			$stmt -> bind_param("s", $uuid);
+		if($stmt = $mysqli -> prepare("SELECT * FROM results WHERE id=? AND owner = ?")){
+			$stmt -> bind_param("ss", $uuid, $_SESSION['dbext']);
 			$stmt -> execute();
 			$stmt -> store_result();
 			$numrows = $stmt->num_rows;
-			$stmt -> bind_result($info['id'], $info['firstname'], $info['lastname'], $info['quizuuid'], $info['rawscore'], $info['possiblescore'], $info['percentage'], $info['datestamp'], $info['timestamp'], $info['ip'], $info['house'], $info['session'], $info['object'], $info['flag'], $info['frscore'], $info['frpossible'], $info['frgraded']);
+			$stmt -> bind_result($info['id'], $info['firstname'], $info['lastname'], $info['quizuuid'], $info['rawscore'], $info['possiblescore'], $info['percentage'], $info['datestamp'], $info['timestamp'], $info['ip'], $info['house'], $info['session'], $info['object'], $info['flag'], $info['frscore'], $info['frpossible'], $info['frgraded'], $info['owner']);
 			$stmt -> fetch();
 			$stmt -> close();
 		}
@@ -32,8 +32,8 @@ if(isset($_SESSION["is_admin"])){
 				}
 			}
 			$quiz->setFRGraded();
-			if($stmt = $mysqli -> prepare("UPDATE results SET object=?, frscore=?, frgraded='1' WHERE id=?;")){
-				$stmt -> bind_param("sii", serialize($quiz), $frt, $uuid);
+			if($stmt = $mysqli -> prepare("UPDATE results SET object=?, frscore=?, frgraded='1' WHERE id=? AND owner = ?;")){
+				$stmt -> bind_param("siis", serialize($quiz), $frt, $uuid, $_SESSION['dbext']);
 				$stmt -> execute();
 				$stmt -> close();
 			} else {
