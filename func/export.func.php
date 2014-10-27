256b5e370fc57f73374b37a4f83893308c82060e
Carlos Cerrillos <carlos@cerrillostech.com>
2014-10-27 10:13:33 -0700

    Export all quiz results
    Improved gzcompress

--- func/export.func.php
+++ func/export.func.php
@@ -10,6 +10,7 @@ if(isset($_SESSION["is_admin"]) && isset($_GET['uuid'])){
 		if($stmt = $mysqli->prepare("SELECT quizname FROM `quizzes` WHERE uuid=?;")){
 			$stmt->bind_param("s", $uuid);
 			$stmt->execute();
+			$stmt->store_result();
 			$stmt->bind_result($name);
 			while($stmt->fetch()) {
 				$quizName = $name;
@@ -20,8 +21,33 @@ if(isset($_SESSION["is_admin"]) && isset($_GET['uuid'])){
 				$generatedquiz = $temp -> createQuiz($uuid);
 				header("Content-type: text/plain");
 				header("Content-Disposition: attachment; filename=".$quizName.".polyquiz");
-				echo base64_encode(serialize($generatedquiz));
+				echo gzcompress(serialize($generatedquiz), 4);
 			}
+		} else {
+			echo $mysqli->error;
+		}
+		$mysqli->close();
+	}
+}
+if(isset($_SESSION["is_admin"]) && isset($_GET['quizuuid'])){
+	if(hasPermissions($_GET['quizuuid'])) {
+		$mysqli = new mysqli($db_host, $db_user, $db_password);
+		$mysqli -> select_db($db_name);
+		$uuid = $_GET['quizuuid'];
+		if($stmt = $mysqli->prepare("SELECT object FROM `results` WHERE quizuuid=?;")){
+			$stmt->bind_param("s", $uuid);
+			$stmt->execute();
+			//$stmt->bind_result($fn, $ln, $uuid, $rawscore, $possibleScore, $percentage, $ds, $ts, $ip, $house, $sessionuuid , $quizobject, $frt, $frp,$frgraded, $owner);
+			$stmt->store_result();
+			$stmt->bind_result($object);
+			$array = array();
+			while($stmt->fetch()) {
+				array_push($array, $object);
+			}
+			$stmt->close();
+			header("Content-type: text/plain");
+			header("Content-Disposition: attachment; filename=".$uuid.".polyquizresults");
+			echo gzcompress(serialize($array), 4);
 		} else {
 			echo $mysqli->error;
 		}
