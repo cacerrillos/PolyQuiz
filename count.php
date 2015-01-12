6955bec418a4af09e34da8ca2d11e4228879e587
Carlos Cerrillos <carlos@cerrillostech.com>
2015-01-12 09:32:34 -0800

    count

--- count.php
+++ count.php
@@ -1,6 +1,6 @@
 <?
 #Counts lines of code in .php files
-$string = shell_exec("find . -name '*.php' -not -path './securimage/*' -not -path './phpmyadmin/*' | xargs wc -l");
+$string = shell_exec("find . -name '*.php' -not -path './securimage/*' -not -path './phpmyadmin/*' -not -path './jpgraph/*' | xargs wc -l");
 preg_match_all('/[0-9]+/',$string,$matches);
 echo $matches[0][count($matches[0])-1]." Lines of Code in PolyQuiz!";
 ?>
