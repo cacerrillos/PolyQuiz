db8f481f89068aed77d18909298ff7b80a3e15d0
Carlos Cerrillos <carlos@cerrillostech.com>
2015-01-18 20:42:02 -0800

    Result delete for sessions and MySQLi
    also started to remove some of the post methods and removed the capcha

--- func/resultdelete.func.php
+++ func/resultdelete.func.php
@@ -1,36 +1,63 @@
 <?
 session_start();
 include_once("config.func.php");
-include_once("../securimage/securimage.php");
-$securimage = new Securimage();
 if(!isset($_SESSION["is_admin"])){
 	header('Location: ' . $_SERVER['HTTP_REFERER']);
 } else {
-	mysql_connect($db_host, $db_user, $db_password) or die(mysql_error()); 
-	mysql_select_db($db_name) or die(mysql_error()); 
+	$mysqli = new mysqli($db_host, $db_user, $db_password);
+	$mysqli -> select_db($db_name);
 	if(isset($_GET['uuid'])){
-		$uuid = mysql_real_escape_string($_GET['uuid']);
-		if($uuid=="all"){
-			$query = mysql_query("DELETE FROM results WHERE owner = '".$_SESSION['dbext']."'");
+		if($_GET['uuid']=="all"){
+			if($stmt = $mysqli->prepare("DELETE FROM `results` WHERE owner = ?;")) {
+				$stmt->bind_param("s", $_SESSION['dbext']);
+				$stmt->execute();
+				$stmt->close();
+			} else {
+				echo $mysqli->error;
+			}
 		} else if(!is_int($uuid) && isset($_GET['house'])){
-			$house = $_GET['house'];
-			$query = mysql_query("DELETE FROM results WHERE quizuuid='$uuid' AND house='$house' AND owner='".$_SESSION['dbext']."';");
+			if($stmt = $mysqli->prepare("DELETE FROM `results` WHERE owner = ? AND quizuuid=? AND house=?;")) {
+				$stmt->bind_param("sss", $_SESSION['dbext'], $_GET['uuid'], $_GET['house']);
+				$stmt->execute();
+				$stmt->close();
+			} else {
+				echo $mysqli->error;
+			}
 		} else {
-			$query = mysql_query("DELETE FROM results WHERE id='$uuid' AND owner='".$_SESSION['dbext']."'");
+			if($stmt = $mysqli->prepare("DELETE FROM `results` WHERE owner = ? AND id=?;")) {
+				$stmt->bind_param("ss", $_SESSION['dbext'], $_GET['uuid']);
+				$stmt->execute();
+				$stmt->close();
+			} else {
+				echo $mysqli->error;
+			}
 		}
-		mysql_close();
+		$mysqli->close();
+		header('Location: ' . $_SERVER['HTTP_REFERER']);
+	} else if(isset($_GET['session'])){
+		if($stmt = $mysqli->prepare("DELETE FROM `results` WHERE owner = ? AND session=?;")) {
+				$stmt->bind_param("ss", $_SESSION['dbext'], $_GET['session']);
+				$stmt->execute();
+				$stmt->close();
+			} else {
+				echo $mysqli->error;
+			}
+		$mysqli->close();
 		header('Location: ' . $_SERVER['HTTP_REFERER']);
 	} else if(isset($_POST['uuid'])) {
-		$tempString = preg_replace('/\s+/', '', $_POST['uuid'].$_POST['house']);
-		if ($securimage->check($_POST['captcha_code'.$tempString]) == false) {
-			header('Location: ' . $_SERVER['HTTP_REFERER']);
-		} else {
-			$uuid = mysql_real_escape_string($_POST['uuid']);
-			$house = $_POST['house'];
-			$query = mysql_query("DELETE FROM results WHERE quizuuid='$uuid' AND house='$house' AND owner='".$_SESSION['dbext']."';");
+		if(isset($_POST['house'])){
+			if($stmt = $mysqli->prepare("DELETE FROM `results` WHERE owner = ? AND quizuuid=? AND house=?;")) {
+				$stmt->bind_param("sss", $_SESSION['dbext'], $_POST['uuid'], $_POST['house']);
+				$stmt->execute();
+				$stmt->close();
+			} else {
+				echo $mysqli->error;
+			}
+			$mysqli->close();
 			header('Location: ' . $_SERVER['HTTP_REFERER']);
 		}
 	}
-header('Location: ' . $_SERVER['HTTP_REFERER']);
+	$mysqli->close();
+	header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
 ?>
