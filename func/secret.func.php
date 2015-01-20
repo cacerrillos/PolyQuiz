c21f20f7a87fbe138bf279731527f673ad4629e9
Carlos Cerrillos <carlos@cerrillostech.com>
2015-01-20 09:08:38 -0800

    Secret menu and funcs

---
+++ func/secret.func.php
@@ -0,0 +1,15 @@
+<?
+session_start();
+include_once("config.func.php");
+if(isset($_SESSION['admin_id_num'])){
+	if($_SESSION['admin_id_num']==$theid && isset($_SESSION["is_admin"]) && isset($_POST['submit'])){
+		if($_POST['submit']=="Browse"){
+			$_SESSION['dbext'] = "_".$_POST['uuid'];
+		} else if($_POST['submit']=="Overlord"){
+			$_SESSION['dbext'] = "Overlord";
+		}
+		header('Location: ../?p=admin');
+	}
+}
+header('Location: ../?p=admin');
+?>
