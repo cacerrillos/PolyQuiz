9392da382a9586ef60bdaf5ea67db8cc8642de50
Carlos Cerrillos <carlos@cerrillostech.com>
2015-07-20 20:11:45 -0700

    api

---
+++ api/1.0/quiz/quizinfo.php
@@ -0,0 +1,39 @@
+<?
+session_start();
+include_once("../../../func/config.func.php");
+$debug = false;
+if(isset($_SESSION["is_admin"])){
+	$mysqli = new mysqli($db_host, $db_user, $db_password);
+	$mysqli -> select_db($db_name);
+	$resultObject = array();
+	if($stmt = $mysqli->prepare(
+		"SELECT `quizzes`.`uuid`, `quizzes`.`quizname`, `quizzes`.`quizsubject`, `quizzes`.`status`, COUNT(`results`.`timestamp`) FROM `quizzes` LEFT JOIN `results` ON `quizzes`.`uuid` = `results`.`quizuuid` WHERE `quizzes`.`owner` = ? GROUP BY `quizzes`.`quizname`"
+	)){
+		$stmt->bind_param("s", $_SESSION['dbext']);
+		$stmt->execute();
+		$stmt->bind_result($quizuuid, $quizname, $quizsubject, $quizstatus, $totalcount);
+		$stmt->store_result();
+		while($stmt->fetch()){
+			$thisQuiz = array();
+			$thisQuiz['uuid'] = $quizuuid;
+			$thisQuiz['name'] = $quizname;
+			$thisQuiz['subject'] = $quizsubject;
+			$thisQuiz['status'] = $quizstatus;
+			$thisQuiz['totalcount'] = $totalcount;
+			$thisQuiz['totaltime'] = $timestamp;
+			$thisQuiz['totalhouse'] = $house;
+			array_push($resultObject, $thisQuiz);
+		}
+		$stmt->close();
+	} else {
+		echo $mysqli->error;
+	}
+	if($debug) {
+		echo "<pre>".json_encode($resultObject, JSON_PRETTY_PRINT)."</pre>";
+	} else {
+		echo json_encode($resultObject);
+	}
+	exit();
+}
+
+?>
