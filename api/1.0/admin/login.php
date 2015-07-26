2284a833e0dd28d2df2bc082cb543ddd28278bd4
Carlos Cerrillos <carlos@cerrillostech.com>
2015-07-26 09:13:56 -0700

    login script

---
+++ api/1.0/admin/login.php
@@ -0,0 +1,35 @@
+<?
+session_start();
+session_destroy();
+session_start();
+include_once("../../../func/config.func.php");
+if(count($_POST) > 0){
+	$mysqli = new mysqli($db_host, $db_user, $db_password);
+	$mysqli -> select_db($db_name);
+	if(mysqli_connect_errno()) {
+		echo "Connection Failed: " . mysqli_connect_errno();
+		exit();
+	}
+	if($stmt = $mysqli -> prepare("SELECT * FROM users WHERE username = ? AND password = ?")){
+		$stmt -> bind_param("ss", $_POST['user'], md5($_POST['pass']));
+		$stmt -> execute();
+		$stmt -> bind_result($data['id'], $data['username'], $data['passwordHash'], $data['permsid']);
+		$stmt -> store_result();
+		$stmt -> fetch();
+		$num = $stmt -> num_rows;
+		$stmt -> close();
+	} else {
+		echo $mysqli->error;
+	}
+	$mysqli -> close();
+	if($num==1){
+		$_SESSION['is_admin'] = "set";
+		$_SESSION['username'] = $_POST['user'];
+		$_SESSION['dbext'] = $data['permsid'];
+		$_SESSION['admin_id_num'] = $data['id'];
+		$_SESSION['permsid'] = $data['permsid'];
+		header('Location: ' . $_SERVER['HTTP_REFERER']);
+	}
+}
+header('Location: ' . $_SERVER['HTTP_REFERER']);
+?>
