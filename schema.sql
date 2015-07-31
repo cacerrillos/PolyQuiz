70abc4665bd63b3fd14c3a72211a6906687c71ab
Carlos Cerrillos <carlos@cerrillostech.com>
2015-07-31 14:50:41 -0700

    new schema

--- schema.sql
+++ schema.sql
@@ -7,123 +7,90 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 
-CREATE TABLE IF NOT EXISTS `globalsession` (
-  `uuid` varchar(64) NOT NULL,
-  `database` varchar(64) DEFAULT NULL,
-  PRIMARY KEY (`uuid`),
-  KEY `database_index` (`database`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE IF NOT EXISTS `images` (
-  `uuid` varchar(65) NOT NULL,
-  `url` text,
-  PRIMARY KEY (`uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE IF NOT EXISTS `polysessions` (
-  `id` varchar(65) NOT NULL,
-  `recoveryid` varchar(16) NOT NULL,
-  `recoverykey` varchar(16) NOT NULL,
-  `data` longtext NOT NULL,
-  `date` varchar(16) DEFAULT NULL,
-  `owner` varchar(64) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `owner_index` (`owner`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE IF NOT EXISTS `quizobjects` (
-  `id` int(11) NOT NULL,
-  `object` longtext,
-  `owner` varchar(64) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `owner_index` (`owner`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 CREATE TABLE IF NOT EXISTS `quizzes` (
+  `uuid` int(11) NOT NULL,
+  `quizname` varchar(255) NOT NULL,
+  `version` int(11) NOT NULL,
+  `owner` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `quizzes_questions` (
   `uuid` varchar(255) NOT NULL,
-  `quizname` text NOT NULL,
-  `quizsubject` text,
-  `status` int(11) DEFAULT '1',
-  `owner` varchar(64) NOT NULL,
-  PRIMARY KEY (`uuid`),
-  KEY `owner_index` (`owner`)
+  `quiz` int(11) NOT NULL,
+  `data` mediumtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `results` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` text,
-  `lastname` text,
-  `quizuuid` varchar(64) DEFAULT NULL,
-  `rawscore` float DEFAULT NULL,
-  `possiblescore` float DEFAULT NULL,
-  `percentage` float DEFAULT NULL,
-  `datestamp` varchar(16) DEFAULT NULL,
-  `timestamp` int(11) DEFAULT NULL,
-  `ip` varchar(32) DEFAULT NULL,
-  `house` enum('North','South','East','West','Other') DEFAULT NULL,
-  `session` varchar(16) DEFAULT NULL,
-  `flag` enum('yes','no') DEFAULT 'no',
-  `frscore` float DEFAULT '0',
-  `frpossible` float DEFAULT '0',
-  `frgraded` float DEFAULT '1',
-  `owner` varchar(64) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `quizuuid_index` (`quizuuid`),
-  KEY `owner_index` (`owner`),
-  KEY `session_index` (`session`),
-  KEY `house_index` (`house`),
-  KEY `flag_index` (`flag`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4530 ;
-
-CREATE TABLE IF NOT EXISTS `sessions` (
-  `uuid` varchar(16) NOT NULL,
-  `key` varchar(16) DEFAULT NULL,
-  `house` enum('North','South','East','West','Other') DEFAULT NULL,
-  `status` int(11) DEFAULT NULL,
-  `quiz` varchar(64) DEFAULT NULL,
-  `date` varchar(16) DEFAULT NULL,
-  `sessionname` text,
-  `owner` varchar(64) DEFAULT NULL,
-  `score` int(11) DEFAULT '1',
-  PRIMARY KEY (`uuid`),
-  KEY `owner_index` (`owner`),
-  KEY `quiz_index` (`quiz`)
+  `id` int(11) NOT NULL,
+  `timestamp` int(11) NOT NULL,
+  `data` mediumtext NOT NULL,
+  `quiz` int(11) NOT NULL,
+  `session` int(11) NOT NULL,
+  `owner` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `stats` (
-  `id` varchar(64) NOT NULL,
-  `value` text,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS `results_responses` (
+  `id` bigint(20) unsigned NOT NULL,
+  `resultid` int(11) NOT NULL,
+  `questionuuid` varchar(64) NOT NULL,
+  `data` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `uuid` int(11) NOT NULL,
+  `sessionid` varchar(32) NOT NULL,
+  `sessionkey` varchar(32) NOT NULL,
+  `quiz` int(11) NOT NULL,
+  `owner` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `username` varchar(64) NOT NULL,
-  `password` varchar(64) NOT NULL,
-  `permissionsid` varchar(64) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `permissionsid` (`permissionsid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+  `password` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
-ALTER TABLE `globalsession`
-  ADD CONSTRAINT `globalsession_ibfk_1` FOREIGN KEY (`database`) REFERENCES `users` (`permissionsid`) ON UPDATE CASCADE;
-
-ALTER TABLE `polysessions`
-  ADD CONSTRAINT `polysessions_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `users` (`permissionsid`) ON UPDATE CASCADE;
-
-ALTER TABLE `quizobjects`
-  ADD CONSTRAINT `quizobjects_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `users` (`permissionsid`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE `quizzes`
-  ADD CONSTRAINT `quizzes_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `users` (`permissionsid`) ON UPDATE CASCADE;
+  ADD PRIMARY KEY (`uuid`);
+
+ALTER TABLE `quizzes_questions`
+  ADD PRIMARY KEY (`uuid`,`quiz`),
+  ADD KEY `quiz` (`quiz`);
 
 ALTER TABLE `results`
-  ADD CONSTRAINT `results_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `users` (`permissionsid`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `results_responses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `questionuuid` (`questionuuid`),
+  ADD KEY `resultid` (`resultid`);
 
 ALTER TABLE `sessions`
-  ADD CONSTRAINT `sessions_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `users` (`permissionsid`) ON UPDATE CASCADE;
+  ADD PRIMARY KEY (`uuid`);
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+
+ALTER TABLE `quizzes`
+  MODIFY `uuid` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `results`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `results_responses`
+  MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
+ALTER TABLE `sessions`
+  MODIFY `uuid` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `quizzes_questions`
+  ADD CONSTRAINT `quizzes_questions_ibfk_1` FOREIGN KEY (`quiz`) REFERENCES `quizzes` (`uuid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `results_responses`
+  ADD CONSTRAINT `results_responses_ibfk_2` FOREIGN KEY (`questionuuid`) REFERENCES `quizzes_questions` (`uuid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `results_responses_ibfk_1` FOREIGN KEY (`resultid`) REFERENCES `results` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
