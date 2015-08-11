31d728a3d22719310db1fac9f1dd1e85a93d3dde
Carlos Cerrillos <carlos@cerrillostech.com>
2015-08-10 17:15:34 -0700

    session create api

---
+++ api/1.0/sessions/create.php
@@ -0,0 +1,39 @@
+<?
+session_start();
+include_once("../../../func/config.func.php");
+//include_once("../../../func/genquiz.func.php");
+include_once("../objects/polyquiz.php");
+$status = array();
+$status['status'] = false;
+if(isset($_SESSION["is_admin"]) && isset($_GET['sessionname']) && isset($_GET['quiz']) && isset($_GET['house']) && isset($_GET['status']) && isset($_GET['show'])){
+	$mysqli = new mysqli($db_host, $db_user, $db_password);
+	$mysqli -> select_db($db_name);
+	
+	$obj = PolySession::createCheckMysql($mysqli);
+	$obj->owner = $_SESSION['dbext'];
+	
+	$obj->name = $_GET['sessionname'];
+	$obj->quiz = $_GET['quiz'];
+	$obj->data['house'] = $_GET['house'];
+	if($_GET['status']=="open"){
+		$obj->setStatus(true);
+	} else if($_GET['status']=="closed"){
+		$obj->setStatus(false);
+	} else {
+		//false
+	}
+	if($_GET['show']=="true"){
+		$obj->setShowScores(true);
+
+	} else if($_GET['show']=="false"){
+		$obj->setShowScores(false);
+	} else {
+		//false
+	}
+	
+	if($obj->saveToMysql($mysqli, $_SESSION['dbext'])){
+		$status['status'] = true;
+	}
+}
+echo json_encode($status);
+?>
