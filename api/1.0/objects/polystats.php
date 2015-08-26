5b67d332984a6a7968204d414a8f940366a5ca39
Carlos Cerrillos <carlos@cerrillostech.com>
2015-08-26 10:05:26 -0700

    FIX THIS

--- api/1.0/objects/polystats.php
+++ api/1.0/objects/polystats.php
@@ -2,9 +2,9 @@
 class PolyStats {
 	public $version = 0;
 	public $data = array();
-	public function __construct($quizUUID, $houseid, $count){
+	public function __construct($quizUUID, $sessionid, $count){
 		$this->data['quizUUID'] = intval($quizUUID);
-		$this->data['houseData'] = intval($houseid);
+		$this->data['sessionData'] = $sessionid;
 		$this->data['count'] = intval($count);
 	}
 	public static function getStats($mysqli, $quiz, $owner, $all = false){
@@ -20,15 +20,15 @@ class PolyStats {
 		} else {
 			die($mysqli->error);
 		}
-		$houses = PolyHouse::getHouses($mysqli, $owner);
+		$sessions = PolySession::ownedBy($mysqli, $owner);
 		for($x = 0; $x < count($houses); $x++){
-			if($stmt = $mysqli->prepare("SELECT COUNT(`id`) FROM `results` WHERE `house` = ? AND `quiz` = ? AND `owner` = ? LIMIT 1;")){
-				$stmt->bind_param("iii", $houses[$x]->data['houseId'], $quiz, $owner);
+			if($stmt = $mysqli->prepare("SELECT COUNT(`id`) FROM `results` WHERE `session` = ? AND `quiz` = ? AND `owner` = ? LIMIT 1;")){
+				$stmt->bind_param("sii", $sessions[$x]->sessionid, $quiz, $owner);
 				$stmt->execute();
 				$stmt->bind_result($count);
 				while($stmt->fetch()){
 					if($all || $count > 0){
-						array_push($resultObject, new self($quiz, $houses[$x]->data['houseId'], $count));
+						array_push($resultObject, new self($quiz, $sessions[$x]->sessionid, $count));
 					}
 				}
 				$stmt->close();
@@ -37,8 +37,8 @@ class PolyStats {
 			}
 		}
 		for($x = 0; $x < count($resultObject); $x++){
-			if($resultObject[$x]->data['houseData'] != -1){
-				$resultObject[$x]->data['houseData'] = PolyHouse::getHouse($mysqli, $resultObject[$x]->data['houseData'], $owner);
+			if($resultObject[$x]->data['sessionData'] != -1){
+				//$resultObject[$x]->data['sessionData'] = PolySession::fromMySQL($mysqli, $resultObject[$x]->data['sessionData'], $owner);
 			}
 		}
 		return $resultObject;
