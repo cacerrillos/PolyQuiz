abfd03a2f5ede8381c1953ab577a2d13a62e5dd6
Carlos Cerrillos <carlos@cerrillostech.com>
2015-10-29 02:56:44 -0700

    get info on specific session id

---
+++ api/1.0/sessions/info.php
@@ -0,0 +1,11 @@
+<?
+session_start();
+include_once("../../../func/config.func.php");
+include_once("../objects/polyquiz.php");
+$debug = false;
+if(isset($_SESSION["is_admin"]) && isset($_GET['uuid'])){
+	$mysqli = new mysqli($db_host, $db_user, $db_password);
+	$mysqli -> select_db($db_name);
+	echo json_encode(PolySession::fromMySQL($mysqli, $_GET['uuid'], $_SESSION['dbext']));
+}
+?>
