c93c1680205c721da7ea92a0a40009cb0561df5d
Carlos Cerrillos <carlos@cerrillostech.com>
2015-10-30 23:30:40 -0700

    remove leaking info for DEBUG

--- public/api/1.0/admin/login.php
+++ public/api/1.0/admin/login.php
@@ -13,9 +13,7 @@ if(count($_POST) > 0){
 	$mysqli = new mysqli($db_host, $db_user, $db_password);
 	$mysqli -> select_db($db_name);
 	if(mysqli_connect_errno()) {
-		$result['status_details'] = "Connection Failed: " . mysqli_connect_errno() . " " . $mysqli->error;
-		$result['dbinfo'] = $db_array;
-		$result['env'] = $_SERVER;
+		$result['status_details'] = "Connection Failed: " . mysqli_connect_errno();
 	} else {
 		if($stmt = $mysqli -> prepare("SELECT `id`, `email`, `name` FROM users WHERE email = ? AND password = ?")){
 			$stmt -> bind_param("ss", $_POST['user'], md5($_POST['pass']));
