d9734b8df5e12ed350030f2494b4bc1083f3570d
Carlos Cerrillos <carlos@cerrillostech.com>
2015-10-30 23:24:03 -0700

    REVERT ME AT SOME POINT, LEAKS DB INFO DEBUG

--- public/api/1.0/admin/login.php
+++ public/api/1.0/admin/login.php
@@ -13,7 +13,8 @@ if(count($_POST) > 0){
 	$mysqli = new mysqli($db_host, $db_user, $db_password);
 	$mysqli -> select_db($db_name);
 	if(mysqli_connect_errno()) {
-		$result['status_details'] = "Connection Failed: " . mysqli_connect_errno();
+		$result['status_details'] = "Connection Failed: " . mysqli_connect_errno() . " " . $mysqli->error;
+		$result['dbinfo'] = $db_array;
 	} else {
 		if($stmt = $mysqli -> prepare("SELECT `id`, `email`, `name` FROM users WHERE email = ? AND password = ?")){
 			$stmt -> bind_param("ss", $_POST['user'], md5($_POST['pass']));
