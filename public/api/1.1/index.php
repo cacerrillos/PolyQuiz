1736ffa11a2a32e2df4e3124c5259f94e6cb369a
Carlos Cerrillos <carlos@cerrillostech.com>
2015-11-06 05:10:13 -0800

    full house api

--- public/api/1.1/index.php
+++ public/api/1.1/index.php
@@ -1,19 +1,20 @@
 <?
 session_start();
-error_reporting(E_ALL);
+//error_reporting(E_ALL);
 require 'Slim/Slim.php';
 include_once("../../func/config.func.php");
 
 include_once('objects/polyquiz.php');
 \Slim\Slim::registerAutoloader();
 $app = new \Slim\Slim();
-if(isset($_SESSION['is_admin'])) {
-	$mysqli = new mysqli($db_host, $db_user, $db_password);
-	$mysqli -> select_db($db_name);
-} else {
+$mysqli = new mysqli($db_host, $db_user, $db_password);
+$mysqli -> select_db($db_name);
 
+$_POST_JSON = json_decode(file_get_contents("php://input"), true);
+
+function isAdmin() {
+	return isset($_SESSION['is_admin']);
 }
-
 if(isset($_SESSION["is_admin"]) && isset($_GET['uuid'])){
 	
 	
@@ -21,18 +22,106 @@ if(isset($_SESSION["is_admin"]) && isset($_GET['uuid'])){
 
 $app->get('/houses', function() {
 	global $mysqli;
-	echo json_encode(PolyHouse::getHouses($mysqli, $_SESSION['dbext']));
+	echo json_encode(PolyHouse::getAll($mysqli, $_SESSION['dbext']));
 });
 
 $app->get('/houses/:houseid', function ($houseid) {
 	global $mysqli;
-	echo json_encode(PolyHouse::getHouse($mysqli, $houseid, $_SESSION['dbext']));
+	echo json_encode(PolyHouse::get($mysqli, $houseid, $_SESSION['dbext']));
+});
+
+$app->get('/status', function() {
+	$result = array();
+	if(isset($_SESSION["is_admin"])) {
+		$result['admin'] = true;
+	} else {
+		$result['admin'] = false;
+	}
+	echo json_encode($result);
+});
+
+$app->post('/login', function() {
+	global $mysqli;
+	session_destroy();
+	session_start();
+
+	global $_POST_JSON;
+
+	$result = array();
+	$result['status'] = false;
+	$result['user'] = $_POST_JSON['user'];
+	if(count($_POST_JSON) > 0){
+		if(mysqli_connect_errno()) {
+			$result['status_details'] = "Connection Failed: " . mysqli_connect_errno();
+		} else {
+			if($stmt = $mysqli -> prepare("SELECT `id`, `email`, `name` FROM users WHERE email = ? AND password = ?")){
+				$stmt -> bind_param("ss", $_POST_JSON['user'], md5($_POST_JSON['pass']));
+				$stmt -> execute();
+				$stmt -> bind_result($data['id'], $data['email'], $data['name']);
+				$stmt -> store_result();
+				$stmt -> fetch();
+				$num = $stmt -> num_rows;
+				$stmt -> close();
+			} else {
+				$result['status_details'] = $mysqli->error;
+			}
+			if($num==1){
+				$_SESSION['is_admin'] = "set";
+				$_SESSION['email'] = $data['email'];
+				$_SESSION['name'] = $data['name'];
+				$_SESSION['dbext'] = $data['id'];
+				$_SESSION['admin_id_num'] = $data['id'];
+				$_SESSION['permsid'] = $data['permsid'];
+				$result['status'] = true;
+			}
+		}
+	}
+	echo json_encode($result);
+});
+
+$app->post('/logout', function() {
+	session_destroy();
+	$status = array();
+	$status['status'] = true;
+	echo json_encode($status);
 });
 
 $app->post('/houses', function() {
-	var_dump($_POST);
+	global $mysqli;
+
+	global $_POST_JSON;
+
+	if(isset($_POST_JSON['name'])) {
+		echo json_encode(PolyHouse::post($mysqli, $_POST_JSON['name'], $_SESSION['dbext']));
+	} else {
+		$error = array();
+		$error['code'] = 50;
+		$error['message'] = "Invalid Arguments";
+		$error['errors'] = array();
+		$error['errors'][0]['code'] = 10;
+		$error['errors'][0]['field'] = 'name';
+		$error['errors'][0]['message'] = 'Body should contain the field name!';
+		echo json_encode($error);
+	}
 });
 
+$app->put('/houses/:houseid', function($houseid) {
+	global $mysqli;
+
+	global $_POST_JSON;
+
+	if($_POST_JSON && isset($_POST_JSON['name'])) {
+		echo json_encode(PolyHouse::put($mysqli, $houseid, $_POST_JSON['name'], $_SESSION['dbext']));
+	}
+
+});
+
+$app->delete('/houses/:houseid', function($houseid) {
+	global $mysqli;
+	if(isAdmin()) {
+		echo json_encode(PolyHouse::delete($mysqli, $houseid, $_SESSION['dbext']));
+	}
+});
 
 $app->run();
 ?>
--- public/api/1.1/objects/polyhouse.php
+++ public/api/1.1/objects/polyhouse.php
@@ -6,9 +6,9 @@ class PolyHouse {
 		$this->data['houseId'] = $houseid;
 		$this->data['houseName'] = $housename;
 	}
-	public static function getHouse($mysqli, $houseid, $owner){
+	public static function get($mysqli, $houseid, $owner){
 		$toReturn = false;
-		if($stmt = $mysqli->prepare("SELECT `houseid`, `housename` FROM `houses` WHERE `houseid` = ? AND `owner` = ?;")){
+		if($stmt = $mysqli->prepare("SELECT `houseid`, `housename` FROM `houses` WHERE `houseid` = ? AND `owner` = ? LIMIT 1;")){
 			$stmt->bind_param("ii", $houseid, $owner);
 			$stmt->execute();
 			$stmt->bind_result($houseid, $housename);
@@ -26,7 +26,7 @@ class PolyHouse {
 			return $toReturn;
 		}	
 	}
-	public static function getHouses($mysqli, $owner) {
+	public static function getAll($mysqli, $owner) {
 		$resultObject = array();
 		if($stmt = $mysqli->prepare("SELECT `houseid`, `housename` FROM `houses` WHERE `owner` = ?;")){
 			$stmt->bind_param("i", $owner);
@@ -39,6 +39,41 @@ class PolyHouse {
 			$stmt->close();
 		} else {
 			die($mysqli->error);
+		}
+		return $resultObject;
+	}
+	public static function post($mysqli, $housename, $owner) {
+		$resultObject = array();
+		if($stmt = $mysqli->prepare("INSERT INTO `houses` (`housename`, `owner`) VALUES (?, ?);")) {
+			$stmt->bind_param("si", $housename, $owner);
+			$stmt->execute();
+			$stmt->close();
+		} else {
+			$resultObject['error'] = $mysqli->error;
+		}
+		return $resultObject;
+	}
+	public static function put($mysqli, $houseid, $housename, $owner) {
+		$resultObject = array();
+		if($stmt = $mysqli->prepare("UPDATE `houses` SET `housename` = ? WHERE `houses`.`houseid` = ? AND `houses`.`owner` = ? LIMIT 1;")) {
+			$stmt->bind_param("sii", $housename, $houseid, $owner);
+			$stmt->execute();
+			$stmt->close();
+			$resultObject['status'] = true;
+		} else {
+			$resultObject['error'] = $mysqli->error;
+		}
+		return $resultObject;
+	}
+	public static function delete($mysqli, $houseid, $owner) {
+		$resultObject = array();
+		if($stmt = $mysqli->prepare("DELETE FROM `houses` WHERE `houses`.`houseid` = ? AND `houses`.`owner` = ? LIMIT 1;")) {
+			$stmt->bind_param("ii", $houseid, $owner);
+			$stmt->execute();
+			$stmt->close();
+			$resultObject['status'] = true;
+		} else {
+			$resultObject['error'] = $mysqli->error;
 		}
 		return $resultObject;
 	}
