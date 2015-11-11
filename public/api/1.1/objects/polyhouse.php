cd2ffce123450070f09baafae5fb762badaf0161
Carlos Cerrillos <carlos@cerrillostech.com>
2015-11-11 12:58:43 -0800

    house new api

--- public/api/1.1/objects/polyhouse.php
+++ public/api/1.1/objects/polyhouse.php
@@ -9,7 +9,7 @@ class PolyHouse {
 	}
 	public static function get($mysqli, $houseid, $owner){
 		$toReturn = false;
-		if($stmt = $mysqli->prepare("SELECT `houseid`, `housename` FROM `houses` WHERE `houseid` = ? AND `owner` = ? LIMIT 1;")){
+		if($stmt = $mysqli->prepare("SELECT `house_id`, `house_name` FROM `house` WHERE `house_id` = ? AND `user_id` = ? LIMIT 1;")){
 			$stmt->bind_param("ii", $houseid, $owner);
 			$stmt->execute();
 			$stmt->bind_result($houseid, $housename);
@@ -29,7 +29,7 @@ class PolyHouse {
 	}
 	public static function getAll($mysqli, $owner) {
 		$resultObject = array();
-		if($stmt = $mysqli->prepare("SELECT `houseid`, `housename` FROM `houses` WHERE `owner` = ?;")){
+		if($stmt = $mysqli->prepare("SELECT `house_id`, `house_name` FROM `house` WHERE `user_id` = ?;")){
 			$stmt->bind_param("i", $owner);
 			$stmt->execute();
 			$stmt->bind_result($houseid, $housename);
@@ -46,7 +46,7 @@ class PolyHouse {
 	public static function post($mysqli, $housename, $owner) {
 		$resultObject = array();
 		$resultObject['status'] = false;
-		if($stmt = $mysqli->prepare("INSERT INTO `houses` (`housename`, `owner`) VALUES (?, ?);")) {
+		if($stmt = $mysqli->prepare("INSERT INTO `house` (`house_name`, `user_id`) VALUES (?, ?);")) {
 			$stmt->bind_param("si", $housename, $owner);
 			if($stmt->execute()) {
 				$resultObject['status'] = true;
@@ -62,7 +62,7 @@ class PolyHouse {
 	}
 	public static function put($mysqli, $houseid, $housename, $owner) {
 		$resultObject = array();
-		if($stmt = $mysqli->prepare("UPDATE `houses` SET `housename` = ? WHERE `houses`.`houseid` = ? AND `houses`.`owner` = ? LIMIT 1;")) {
+		if($stmt = $mysqli->prepare("UPDATE `house` SET `house_name` = ? WHERE `house`.`house_id` = ? AND `house`.`user_id` = ? LIMIT 1;")) {
 			$stmt->bind_param("sii", $housename, $houseid, $owner);
 			$stmt->execute();
 			$stmt->close();
@@ -74,7 +74,7 @@ class PolyHouse {
 	}
 	public static function delete($mysqli, $houseid, $owner) {
 		$resultObject = array();
-		if($stmt = $mysqli->prepare("DELETE FROM `houses` WHERE `houses`.`houseid` = ? AND `houses`.`owner` = ? LIMIT 1;")) {
+		if($stmt = $mysqli->prepare("DELETE FROM `house` WHERE `house`.`house_id` = ? AND `house`.`user_id` = ? LIMIT 1;")) {
 			$stmt->bind_param("ii", $houseid, $owner);
 			if($stmt->execute()) {
 				if($stmt->affected_rows == 0) {
