f2247e718098e74baeafafd8616fe627c75bbc07
Carlos Cerrillos <carlos@cerrillostech.com>
2015-11-13 10:00:20 -0800

    get quizzes api changes

--- public/api/1.1/poly_quiz_api.php
+++ public/api/1.1/poly_quiz_api.php
@@ -1,12 +1,21 @@
 <?php
 $app->get('/quizzes', function() {
   global $mysqli;
-  if(isset($_GET['id'])) {
-    echo json_encode(PolyQuiz::from_mysql($mysqli, $_GET['id'], $_SESSION['dbext']), JSON_PRETTY_PRINT);
+  if(isset($_GET['quiz_id'])) {
+    get_quiz($mysqli, $_GET['quiz_id'], $_SESSION['dbext'], isset($_GET['all_data']));
   } else {
     echo json_encode(PolyQuiz::all_from_mysql($mysqli, $_SESSION['dbext']), JSON_PRETTY_PRINT);
   }
 });
+
+$app->get('/quizzes/:quizid', function($quizid) {
+  global $mysqli;
+  get_quiz($mysqli, $quizid, $_SESSION['dbext'], isset($_GET['all_data']));
+});
+
+function get_quiz($mysqli, $quiz_id, $user_id, $all_data) {
+  echo json_encode(PolyQuiz::from_mysql($mysqli, $quiz_id, $user_id, $all_data), JSON_PRETTY_PRINT);
+}
 
 $app->delete('/quizzes/:quizid', function($quizid) {
   global $mysqli;
