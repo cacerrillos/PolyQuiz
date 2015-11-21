5a6a2c61edc02a46ac7dc58d16be8a0d8711adca
Carlos Cerrillos <carlos@cerrillostech.com>
2015-11-20 21:03:17 -0800

    tempbugffix

--- public/api/1.1/poly_answer_api.php
+++ public/api/1.1/poly_answer_api.php
@@ -1,7 +1,7 @@
 <?php
 $app->get('/answers/:answerid', function($answerid) {
   global $mysqli;
-  $answer = PolyAnswer
+  //$answer = PolyAnswer
   echo "getting " . $answerid . " owned by " . $_SESSION['dbext'];
 });
 ?>
