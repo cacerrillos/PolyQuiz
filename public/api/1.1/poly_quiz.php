a120357a282fd0e4993f8415fb9aac9dd46c1936
Carlos Cerrillos <carlos@cerrillostech.com>
2015-11-26 20:39:35 -0800

    use factory

--- public/api/1.1/poly_quiz.php
+++ public/api/1.1/poly_quiz.php
@@ -72,9 +72,9 @@ class PolyQuiz {
     return $result;
   }
   public function fetch_questions_from_mysql($mysqli, $user_id) {
-    $fetch_question_standard = PolyQuestion_Standard::all_from_mysql($mysqli, $this->quiz_id, $user_id);
-    if($fetch_question_standard['status']) {
-      $this->questions = $this->questions + $fetch_question_standard['result'];
+    $questions = PolyQuestionFactory::get_by_quiz($mysqli, $this->quiz_id, $user_id);
+    if($questions) {
+      $this->questions = $this->questions + $questions;
     }
   }
   public static function create($mysqli, $quiz_name, $user_id) {
