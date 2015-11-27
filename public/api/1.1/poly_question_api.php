5d0dd778c5b418e93af24e69db7f85c15087f092
Carlos Cerrillos <carlos@cerrillostech.com>
2015-11-26 18:58:22 -0800

    create question via API

--- public/api/1.1/poly_question_api.php
+++ public/api/1.1/poly_question_api.php
@@ -1,6 +1,14 @@
 <?
 $app->post('/questions', function() {
   global $_POST_JSON, $mysqli;
-  echo json_encode($_POST_JSON, JSON_PRETTY_PRINT);
+  $quiz = PolyQuiz::from_mysql($mysqli, $_POST_JSON['quiz_id'], $_SESSION['dbext']);
+  if($_POST_JSON['type'] == "STANDARD") {
+    $question = new PolyQuestion_Standard($quiz['result']);
+    $question->text = $_POST_JSON['text'];
+    $question->extra_credit = $_POST_JSON['extra_credit'];
+    $question->canvas = $_POST_JSON['canvas'];
+    $res = $question->save($mysqli, $_SESSION['dbext']);
+  }
+  echo json_encode($res, JSON_PRETTY_PRINT);
 });
 ?>
