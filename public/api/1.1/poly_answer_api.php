99d9ae76bd5abd9f1618b3d724f8c2d7789c0b2e
Carlos Cerrillos <carlos@cerrillostech.com>
2015-12-22 14:50:48 -0800

    DELETE /answers endpoint

--- public/api/1.1/poly_answer_api.php
+++ public/api/1.1/poly_answer_api.php
@@ -4,4 +4,20 @@ $app->get('/answers/:answerid', function($answerid) {
   //$answer = PolyAnswer
   echo json_encode(PolyAnswerFactory::get($mysqli, $answerid, $_SESSION['dbext']), JSON_PRETTY_PRINT);
 });
+
+$app->delete('/answers', function() {
+  global $mysqli;
+  $result = false;
+  if(isset($_GET['answer_id'])) {
+    $result = PolyAnswerFactory::delete($mysqli, $_GET['answer_id'], $_SESSION['dbext']);
+  }
+  echo json_encode($result, JSON_PRETTY_PRINT);
+});
+
+$app->delete('/answers/:answerid', function($answerid) {
+  global $mysqli;
+  $result = PolyAnswerFactory::delete($mysqli, $answerid, $_SESSION['dbext']);
+  echo json_encode($result, JSON_PRETTY_PRINT);
+});
+
 ?>
