1546243df1ab0259614bac65f948416a9870c4b7
Carlos Cerrillos <carlos@cerrillostech.com>
2017-03-28 00:23:43 -0700

    Clean slate

--- public/api/1.0/admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?
-	session_start();
-	session_destroy();
-	$status = array();
-	$status['status'] = true;
-	echo json_encode($status);
-?>
--- public/func/count.func.php
+++
@@ -1,9 +0,0 @@
-<?
-#Counts lines of code in .php files
-function lines(){
-	$string = shell_exec("find . -name '*.php' -not -path './securimage/*' -not -path './phpmyadmin/*' -not -path './jpgraph/*' | xargs wc -l");
-	preg_match_all('/[0-9]+/',$string,$matches);
-	$linesofcode = $matches[0][count($matches[0])-1];
-	return $linesofcode;
-}
-?>
